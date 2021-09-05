e5caecc1e2ac7d2234443f87990ea1a1730ef43d
Pranjal Mahajan <pranjalmahajan96@gmail.com>
2021-09-05 20:32:34 +0530

    style

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,18 +11,18 @@ export const Navbar = () => {
             </NavLink>
           </div>
   
-          {/* <ul className="list-non-bullet nav-pills">
+          <ul className="list-non-bullet nav-pills">
             <li className="list-item-inline">
               <NavLink to="/login" className="link link-active">
-                Login
+               
               </NavLink>
             </li>
             <li className="list-item-inline">
               <NavLink to="/login" className="link link-active">
-                User
+                
               </NavLink>
             </li>
-          </ul> */}
+          </ul>
         </nav>
       </div>
     );
